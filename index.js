e5a3f389be966d7d8e44c14bca814bc06c87d909
erikn775 <erikn775@gmail.com>
2021-04-19 14:32:58 -0600

    commit tree 581239a59e5c119fea34c4826b94d5a5a577ce34

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = sourceArray.map(array => array * -1);
+    return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = sourceArray.map(array => array);
+    return newArray;
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = sourceArray.map(array => array * 2);
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = sourceArray.map(array => array**2);
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return sourceArray.reduce(reducer, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+    const reducer = (accumulator, currentValue) => {
+        if(!accumulator) return false;
+    }
+    return sourceArray.reduce(reducer)
+}
